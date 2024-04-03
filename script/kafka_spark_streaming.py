396ce09cfd33fcd0255004912000dac060b46c51
PallaviKhedle <pallavi8khedle@gmail.com>
2024-04-02 19:31:57 -0700

    SparkSession created

--- script/kafka_spark_streaming.py
+++ script/kafka_spark_streaming.py
@@ -2,40 +2,46 @@ from pyspark.sql import SparkSession
 import logging
 from config import KAFKA_TOPIC, BOOTSTRAP_SERVERS
 
-
 # Set up logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+# Log the Kafka bootstrap servers configuration
+logger.info(f"Kafka bootstrap servers: {BOOTSTRAP_SERVERS}")
+
 # Create SparkSession with custom configurations
 spark = SparkSession.builder \
     .appName("KafkaSparkStreamingApp") \
-    .config("spark.kafka.bootstrap.servers", "3.145.153.231:9092") \
-    .config("spark.kafka.consumer.key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer") \
-    .config("spark.kafka.consumer.value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer") \
-    .config("spark.kafka.consumer.auto.offset.reset", "latest") \
-    .config("spark.kafka.consumer.enable.auto.commit", "true") \
+    .config("spark.kafka.bootstrap.servers", BOOTSTRAP_SERVERS) \
     .getOrCreate()
 
-# Kafka configuration
-kafka_servers = BOOTSTRAP_SERVERS # Kafka broker address
-kafka_topic=KAFKA_TOPIC
+# Check if SparkSession is created
+if spark:
+    logger.info("SparkSession created successfully")
+else:
+    logger.error("Failed to create SparkSession")
 
 # Define input DataFrame using Kafka source
 df = spark \
     .readStream \
     .format("kafka") \
-    .option("kafka.bootstrap.servers", kafka_servers) \
-    .option("subscribe", kafka_topic) \
+    .option("kafka.bootstrap.servers", BOOTSTRAP_SERVERS) \
+    .option("subscribe", KAFKA_TOPIC) \
     .load()
 
-# Perform processing and print message content
+# Log Kafka topic
+logger.info(f"Subscribed to Kafka topic: {KAFKA_TOPIC}")
+
+# Perform processing, log message content, and display on console
 query = df \
-    .selectExpr("CAST(value AS STRING)") \
+    .selectExpr("CAST(value AS STRING) AS message") \
     .writeStream \
-    .foreach(lambda row: logger.info(row.asDict())) \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", BOOTSTRAP_SERVERS) \
+    .option("topic",KAFKA_TOPIC) \
+    .foreach(logger.info) \
     .outputMode("append") \
     .start()
 
-# Wait for termination
+# Add another logger to display data on the console
 query.awaitTermination()
