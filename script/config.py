0e4bc64dd5ec2c05ed75bff9a30dc4224ce7f1e3
tejashwatiwari <tejashwatiwari@gmail.com>
2024-05-12 16:45:24 -0700

    changes

--- script/config.py
+++ script/config.py
@@ -7,7 +7,7 @@ BOOTSTRAP_SERVERS = '3.145.153.231:9092'
 # KAFKA_TOPIC = 'streamAnalysis'
 KAFKA_TOPIC = 'testingSteam'
 
-Access_key="AKIA47CRU5OFBMMMZGJW"
-Secret_key="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"
+Access_key="Access_key"
+Secret_key="Secret_key"
 
 
